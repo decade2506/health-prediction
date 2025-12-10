3ba487b10b4ce7ef856f7f793f26afdef936b3f6
Quan Nguyen <minhquan10tin@gmail.com>
2025-12-10 15:40:36 +0700

    split data to train and test

--- src/model.py
+++ src/model.py
@@ -1,7 +1,7 @@
 import pandas as pd
-import modules.logistic
-import modules.scaler
-import modules.testSplit
+import modules.logistic as l
+import modules.scaler as s
+import modules.testSplit as ts
 # only use these imports for saving files and testing
 from joblib import dump
 from sklearn.metrics import accuracy_score, classification_report
@@ -9,3 +9,7 @@ from sklearn.metrics import accuracy_score, classification_report
 data_frame = pd.read_csv("./heart_failure_clinical_records_dataset.csv")
 X = data_frame.drop("DEATH_EVENT", axis=1)
 Y = data_frame["DEATH_EVENT"]
+
+X_train, Y_train, X_test, Y_test = ts.train_test(X.values.tolist(), Y.values.tolist(), test_size=0.3)
+
+w,b = l.logisticRegression(X_train, Y_train, lr=0.01, steps=1000)
