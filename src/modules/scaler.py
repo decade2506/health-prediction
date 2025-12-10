78fd2ec918f7dba43aa90a34f0a1a3e3b4f2b711
Quan Nguyen <minhquan10tin@gmail.com>
2025-12-10 17:16:35 +0700

    smt smt ...

--- src/modules/scaler.py
+++ src/modules/scaler.py
@@ -5,13 +5,12 @@ class MyScaler: #Scaler is so confusing
     def __init__(self):
         self.mean = None
         self.std = None
-    def fit(self, x): #Chuẩn hóa từ 0 đến 1
-        x = list(x)
-        self.mean = np.mean(x)
-        self.std = np.std(x)
+    def fit(self, x):
+        x = np.array(x, dtype=float)
+        self.mean = np.mean(x, axis=0)
+        self.std = np.std(x, axis=0)
         return self
     def transform(self, x):
         return (x - self.mean) / self.std
     def fit_transform(self, x):
-        self.fit(x)
-        return self.transform(x)
+        return self.fit(x).transform(x)
